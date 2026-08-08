629a02164f8d0551cd9a41c0ebdfba140be8bbf5
Priscila Mantovani <APRISCILAMANTOVANI@GMAIL.COM>
2026-08-07 21:34:38 -0700

    Melhoria G: remove pasta flore_flutter/flore_flutter duplicada
    Debito tecnico do commit 89c3913: pasta duplicada confundia build e
    tinha dependencias divergentes das do projeto real em flore_flutter/.

--- flore_flutter/flore_flutter/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:flore_flutter/main.dart';
-
-void main() {
-  testWidgets('App smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const FloreApp());
-    expect(find.byType(FloreApp), findsOneWidget);
-  });
-}
