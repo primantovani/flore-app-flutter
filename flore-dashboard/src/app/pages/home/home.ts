22192c8783d2f7483a370de9978139608e806510
sophsmoreau <sophiemoreaup@icloud.com>
2026-08-17 21:27:07 -0300

    Adiciona conteudo real a tela Home do dashboard

--- flore-dashboard/src/app/pages/home/home.ts
+++ flore-dashboard/src/app/pages/home/home.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+﻿import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrl: './home.css'
 })
-export class Home {}
+export class Home {
+
+}
